9dfaf0f60918d95a1c15daadd6ecc78f631bf57b
halcyon0917 <atdofficial1723@gmail.com>
2024-07-02 14:01:10 +0800

    token setter

---
+++ app/components/TokenSetter.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { setCookie } from "cookies-next";
+import { LegacyRef, useRef } from "react";
+
+export default function TokenSetter() {
+  const inputRef = useRef<HTMLInputElement>();
+  const handleSetToken = () => {
+    const value = inputRef.current?.value || "token-value-from-dot-vam-domain";
+    setCookie("token", value, { domain: "adriantdc.com" });
+  };
+  return (
+    <div className='flex gap-2'>
+      <input ref={inputRef as unknown as LegacyRef<HTMLInputElement>} type='text' className='text-black px-2' placeholder='value' />
+      <button onClick={handleSetToken} className='p-2 bg-white text-black'>
+        Set Token
+      </button>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
-import { cookies } from "next/headers";
-import Image from "next/image";
-import HomePage from "./page-components/HomePage";
+import TokenSetter from "./components/TokenSetter";
 
 export default function Home() {
-  return <HomePage />;
+  return (
+    <main className='p-4 m-auto max-w-xl'>
+      <h1 className='text-5xl'>VAM (versa-vam)</h1>
+      <p>
+        domain: <span className='font-bold text-blue-400'>vam.adriantdc.com</span>
+      </p>
+      <TokenSetter />
+    </main>
+  );
 }
